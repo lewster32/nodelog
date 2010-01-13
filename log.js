bdcb2fc6d37211f38bd7cac232f5df863b2f0b08
Lew <lew@ubuntu.ubuntu-domain>
2010-01-13 23:41:57 +0000

    Added colourised messages via consolelog.node.js - http://github.com/jazzychad/consolelog.node.js Removed automatic joining to allow users to specify join command in perform Differentiate between channel messages and queries (private messages) Added (but commented out) notice support, requires better regexp matching. TODO: Fix up the irc.js module to output only properly validated JSON-like objects

--- log.js
+++ log.js
@@ -1,14 +1,14 @@
-process.mixin(require('sys'));
-var config = require('./config');
+var config = require('./config'),
+	tc = require('./lib/consolelog/termcolors'),
+	irc = require('./lib/irc');
 
-var irc = require('./lib/irc');
-var file = require('file');
-var path = require('path');
-var repl = require('repl');
-var sys = require('sys');
+var file = require('file'),
+	path = require('path'),
+	repl = require('repl'),
+	sys = require('sys');
 
 var logFile, day;
-function writeLog(text) {
+function writeLog(text, color) {
 
   text = text.replace(/((\x03)([0-9]*)(,?)([0-9]+))|[^(\x20-\xFF)]*/g,''); // strip out mIRC colour codes and other non-printable chars
   var date = new Date;
@@ -31,7 +31,13 @@ function writeLog(text) {
   ].join(':');
   
   logFile.write('('+time+') '+text+"\n");
+  
+  if ((typeof color == 'number') && (color >= 0) && (color <= 9)) {
+    tc.terminalFontReset();
+	tc.terminalSetFont(color, 9, 9);
+  }
   sys.puts('('+time+') '+text);
+  tc.terminalFontReset();
 }
 
 var client = new irc.Client(config.host, config.port);
@@ -43,23 +49,22 @@ client.addListener('001', function() {
 		this.send(config.perform[p]);
 	}
   }
-  this.send('JOIN', config.channel);
 });
 
 client.addListener('JOIN', function(prefix, channel) {
-  writeLog('* Joins: '+irc.user(prefix).nick+' ('+irc.user(prefix).user+'@'+irc.user(prefix).host+')');
+  writeLog('* Joins: '+irc.user(prefix).nick+' ('+irc.user(prefix).user+'@'+irc.user(prefix).host+')', 2);
 });
 
 client.addListener('PART', function(prefix, channel) {
-  writeLog('* Parts: '+irc.user(prefix).nick+' ('+irc.user(prefix).user+'@'+irc.user(prefix).host+')');
+  writeLog('* Parts: '+irc.user(prefix).nick+' ('+irc.user(prefix).user+'@'+irc.user(prefix).host+')', 4);
 });
 
 client.addListener('QUIT', function(prefix, message) {
-  writeLog('* Quits: '+irc.user(prefix).nick+' ('+irc.user(prefix).user+'@'+irc.user(prefix).host+') ('+message+')');
+  writeLog('* Quits: '+irc.user(prefix).nick+' ('+irc.user(prefix).user+'@'+irc.user(prefix).host+') ('+message+')', 4);
 });
 
 client.addListener('KICK', function(prefix, channel, nick, message) {
-  writeLog('* '+nick+' was kicked by '+irc.user(prefix).nick+' ('+message+')');
+  writeLog('* '+nick+' was kicked by '+irc.user(prefix).nick+' ('+message+')', 4);
   if (nick == config.nick) {
 	this.send('JOIN', config.channel);
   }
@@ -67,12 +72,18 @@ client.addListener('KICK', function(prefix, channel, nick, message) {
 
 client.addListener('MODE', function(prefix, channel, modes, target) {
   target ? target = ' '+target : target = '';
-  writeLog('* '+irc.user(prefix).nick+' sets mode: '+modes+target);
+  writeLog('* '+irc.user(prefix).nick+' sets mode: '+modes+target, 2);
 });
 
 client.addListener('TOPIC', function(prefix, channel, text) {
-  writeLog('* '+irc.user(prefix).nick+' changes topic to \''+text+'\'');
+  writeLog('* '+irc.user(prefix).nick+' changes topic to \''+text+'\'', 1);
 });
+
+/*
+client.addListener('NOTICE', function(prefix, message) {
+  writeLog('-'+irc.user(prefix).nick+'- '+text, 1);
+});
+*/
 
 client.addListener('PRIVMSG', function(prefix, channel, text) {
   switch (text) {
@@ -82,7 +93,10 @@ client.addListener('PRIVMSG', function(prefix, channel, text) {
       break;
   }
   if (text.match(/^\x01ACTION\s/)) {
-	writeLog('* '+irc.user(prefix).nick+' '+text.replace(/^\x01ACTION\s/,''));
+	writeLog('* '+irc.user(prefix).nick+' '+text.replace(/^\x01ACTION\s/,''), 5);
+  }
+  else if (channel == config.nick) {
+	writeLog('*'+irc.user(prefix).nick+'* '+text, 1);
   }
   else {
 	writeLog('<'+irc.user(prefix).nick+'> '+text);
