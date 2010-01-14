2d6722e2fa87da7b5afa442e29f163faf3d73850
Lew <lew@ubuntu.ubuntu-domain>
2010-01-14 00:01:47 +0000

    Added termcolors via consolelog.node.js (jazzychad) Added differentiation between channel messages and private messages Added notice - commented out due to broken regexp in irc.js TODO: Fix irc.js to output validated and robust data

--- config.template.js
+++ config.template.js
@@ -4,10 +4,10 @@ process.mixin(exports, {
   nick: 'nodelog',
   user: 'nodelog',
   real: 'NodeLog',
-  channel: '#nodejs',
   logUrl: 'http://nodejs.debuggable.com/',
   logPath: 'log',
   perform: [
-	"PRIVMSG NickServ :IDENTIFY password"
+	"PRIVMSG NickServ :IDENTIFY password",
+	"JOIN #nodejs"
   ]
 });
